2a21a9c4d5f61ed357e24e676edaf6bd2d479cc1
mriduldas07 <mriduldas0325@gmail.com>
2022-04-19 03:31:18 +0600

    footer complete

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -3,12 +3,18 @@ import { Col, Container, Row } from 'react-bootstrap';
 import './Footer.css'
 
 const Footer = () => {
+    const getCurrTime = () =>{
+        let newTime = new Date().toLocaleTimeString();
+        return newTime;
+    }
     const getCurrentYear = () =>{
-        return new Date().getFullYear();
+        let newDate = new Date().toDateString();
+        return newDate;
     };
     return (
-        <div className=' footer bg-dark sticky-bottom'>
+        <div className=' footer bg-dark sticky-bottom mt-5'>
             <Container>
+                <h3 className="pt-5 text-center">Local Time: {getCurrTime()}</h3>
                 <Row className='pt-5'>
                     <Col lg={3} sm={6}>
                         <h4 className='pb-3'>Information</h4>
@@ -32,11 +38,15 @@ const Footer = () => {
                         <h6>24/7 support</h6>
                     </Col>
                     <Col lg={3} sm={6}>
-                        
+                        <h4>Address</h4>
+                        <h6>Local brach: Mymensingh, Uttara,Dhanmondi</h6>
+                        <h6>Head Office: Dhanmondi, Dhaka</h6>
+                        <h6>Phone: 01571124907</h6>
+                        <h6>Email: mriduldas0325@gmail.com</h6>
                     </Col>
                     </Row>
             </Container>
-            <p className='text-center mt-5'>copyright @ {getCurrentYear()}</p>
+            <p className='text-center pt-5'>Copyright @ {getCurrentYear()} by Travelers !!</p>
         </div>
     );
 };
