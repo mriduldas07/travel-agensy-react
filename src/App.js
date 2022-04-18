a1fc1663b3bdf3dc4de202f72749f6fef689e189
mriduldas07 <mriduldas0325@gmail.com>
2022-04-18 18:52:27 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './Shared/Header/Header';
 import Home from './Pages/Home/Home/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown,  } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-
-// import banner1 from '../../images/banner/banner1.jpg'
-// import banner2 from '../../images/banner/banner2.jpg'
 
 const Header = () => {
     return (
         <>
-            <h2>header</h2>
-        </>
+  <Navbar collapseOnSelect expand="lg" sticky='top'  variant="dark" style={{backgroundColor: 'gray'}}>
+  <Container>
+  <Navbar.Brand as={Link} to={`/`}>Travelers</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="/home#services">Services</Nav.Link>
+      <Nav.Link href="/home#experts">Blogs</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link as={Link} to="/about" variant="dark">About</Nav.Link>
+      <Nav.Link as={Link} to="/login">
+        Login
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+</>
     );
 };
 
