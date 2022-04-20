91030ab599da92465b872ab8cb77c3ddea77d2ca
mriduldas07 <mriduldas0325@gmail.com>
2022-04-20 15:32:48 +0600

    firebase problem solve and register compo

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 import login from '../../images/login.jpg'
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
     const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value);
+    }
 
     const navigateToLogin = event =>{
         navigate('/login');
     }
     const handleRegister = event =>{
         event.preventDefault();
+        if (password !== confirmPassword) {
+            setError("incoreect");
+            return;
+        }
+        if (password.length < 6) {
+            setError('6 digit');
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
     }
 
     return (
@@ -23,13 +51,13 @@ const Register = () => {
                     <Col lg={5} sm={12}>
                         <h1 className='mt-5 form-title'>Register</h1>
                         <form onSubmit={handleRegister} className='container mx-auto'>
-                            <input type="email" name="email" placeholder='Your Email' required id="" />
+                            <input onBlur={handleEmailBlur} type="email" name="email" placeholder='Your Email' required id="" />
                             <br />
                             <br />
-                            <input type="password" name="password" placeholder='Password' required id="" />
+                            <input onBlur={handlePasswordBlur} type="password" name="password" placeholder='Password' required id="" />
                             <br />
                             <br />
-                            <input type="password" name="confirm-password" placeholder='Confirm Password' required id="" />
+                            <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" placeholder='Confirm Password' required id="" />
                             <p>Already have an account?<Link to={`/login`} className='text-warning text-decoration-none' onClick={navigateToLogin}> Login</Link></p>
                             <input type="submit" value="Register" />
                         </form>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId
+  apiKey: "AIzaSyCERbYqlCv05dIBfrjl5Fj8swx-5zBfGeg",
+  authDomain: "assignment-10-efab3.firebaseapp.com",
+  projectId: "assignment-10-efab3",
+  storageBucket: "assignment-10-efab3.appspot.com",
+  messagingSenderId: "200089427171",
+  appId: "1:200089427171:web:64149462503ed0b0cf39d6"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 
 export default auth;
